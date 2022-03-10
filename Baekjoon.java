50e499649aa9506aa206e28a2d615a1804073186
Ehigh <rkd2rb@gmail.com>
2022-03-10 22:11:24 +0900

    백준 9498번 풀이

--- Baekjoon.java
+++ Baekjoon.java
@@ -5,11 +5,12 @@ import java.io.IOException;
 class Baekjoon {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedReader : 입력값을 일정 길이의 버퍼 단위로 끊어서 불러옴.
         int score = Integer.parseInt(br.readLine());
         
-        char grade;
+        char grade; // 성적을 저장할 변수
         
-        switch (score / 10) {
+        switch (score / 10) { // int형끼리의 연산이므로, 결과값은 소수점 이하는 버려진 int형이다.
             case 10: case 9:
                 grade = 'A';
                 break;

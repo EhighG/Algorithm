f9338df53235209154e80de29253e661c6921e37
Ehigh <rkd2rb@gmail.com>
2022-03-12 22:30:52 +0900

    백준 14681번

---
+++ Q_14681.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q_14681 {
+
+	public static void main(String[] args) throws IOException {
+		// find quadrant
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+		
+		if (y > 0) {
+			if (x > 0) System.out.println(1);
+			else System.out.println(2);
+		} else {
+			if (x > 0) System.out.println(4);
+			else System.out.println(3);
+		}
+		
+
+	}
+
+}

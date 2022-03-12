0ac833843daa0e328131165cfcb3d21e1c31ef2c
Ehigh <71206505+EhighG@users.noreply.github.com>
2022-03-12 22:34:10 +0900

    Update Q_14681.java
    주석 추가

--- Q_14681.java
+++ Q_14681.java
@@ -14,8 +14,8 @@ public class Q_14681 {
 		
 		if (y > 0) {
 			if (x > 0) System.out.println(1);
-			else System.out.println(2);
-		} else {
+			else System.out.println(2); // 문제에 x != 0 이라고 명시되어 있으므로, 같은 경우는 고려하지 않는다.
+		} else { // 윗줄의 x와 마찬가지로, y=0인 경우는 고려하지 않는다.
 			if (x > 0) System.out.println(4);
 			else System.out.println(3);
 		}

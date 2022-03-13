f7a81c64b7d4fce3d6211fcf4a4a33c45c7b7bb1
Ehigh <71206505+EhighG@users.noreply.github.com>
2022-03-13 23:22:34 +0900

    Update Q_2753.java
    if문 오류 수정

--- Q_2753.java
+++ Q_2753.java
@@ -8,13 +8,15 @@ public class Q_2753 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int input = Integer.parseInt(br.readLine());
-		int result = 0; // 기본값 0
+		int result; 
 		
-		if (input % 4 == 0) {
-			// 4의 배수들 중,
-			if (!(input%100==0 && input%400!=0)) {result = 1;}
-			// 윤년이 아닌 것(100의 배수이면서, 400의 배수가 아닌 것)을 제외한 나머지 >> 1
-		}
+
+		// (4의 배수 && 100의 배수가 아닐 때) or 400의 배수일 때
+		if ((input % 4 == 0) && (input % 100 != 0)) {
+			result = 1;
+		} else if (input % 400 == 0) {
+			result = 1;
+		} else {result = 0;}
 		
 		System.out.println(result);
 	}

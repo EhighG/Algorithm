c1a93b1e12fcc1c64bc315092596c913a2710803
Ehigh <rkd2rb@gmail.com>
2022-03-18 23:01:24 +0900

    백준 2581번 - 15956KB, 188ms 사용. 원인 알아보기

---
+++ Q_2581.java
@@ -0,0 +1,36 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_2581 {
+
+	public static void main(String[] args) throws IOException {
+		// M이상 N이하 소수 min, sum 구하기
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int m = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		
+		int i,j;
+		int min = -1;
+		int sum = 0; 
+		int startPoint = Math.max(2, m);
+		
+		Loop1 : for(i = startPoint; i <= n; i++) { // 1은 소수가 아니므로 고려할 필요가 없다.
+		 for(j=1; j<i; j++) {
+			 if (i%j == 0 && j != 1) { // 1이 아닌, 나눠지는 수가 발견될 시
+				 continue Loop1; // 해당 숫자(i)는 소수가 아니므로 Pass.
+			 }
+		 }
+		if (min == -1) min = i; // 처음 발견된 소수라면, min에 저장
+		sum += i;
+		}
+		
+		if(sum==0) {
+			System.out.println(-1);
+		} else {
+			System.out.println(sum+"\n"+min);
+		}
+	}
+}

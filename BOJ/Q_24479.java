6c6e5b6d57f74ea8f864f55bec193de6e7c4e4b1
Ehigh <rkd2rb@naver.com>
2022-09-08 22:41:30 +0900

    그래프와 순회 - dfs(1)

--- BOJ/Q_24479.java
+++ BOJ/Q_24479.java
@@ -3,75 +3,66 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Q_24479 {
+    static int visitOrder = 1;
+
+    static int nV;
+    static int[] visitOrderArr;
+    static boolean[] visitedArr;
+    static ArrayList<Integer>[] dfsGraph;
 
     public static void main(String[] args) throws IOException {
-        /*
-        실패 - 메모리 초과!
-        2차원 배열을 이용한 구현은 두 노드가 연결되어있는지 여부를 찾는 덴 빠르지만, n x n 크기의 배열을 가지므로 공간 효율성이 좋지 않다.
-        문제에 언급된 정점의 수에 따라, 최대 100,001 x 100,001개의 공간이 필요하다.
-        문제에서 요구하는 결과를 구하는 데 있어서도, 리스트로 만든 것이 더 용이하다.
-         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int nV = Integer.parseInt(st.nextToken());
+        nV = Integer.parseInt(st.nextToken());
         int nE = Integer.parseInt(st.nextToken());
         int startV = Integer.parseInt(st.nextToken());
 
-        DfsGraph graph = new DfsGraph(nV);
+        visitOrderArr = new int[nV+1];
+        visitedArr = new boolean[nV+1];
+        dfsGraph = new ArrayList[nV+1];
 
-        // 인접행렬 초기화
-        int x, y;
-        for (int i = 0; i < nE; i++) {
-            st = new StringTokenizer(br.readLine());
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-            graph.put(x, y);
+        for (int i = 0; i <= nV; i++) {
+            dfsGraph[i] = (new ArrayList<Integer>());
         }
 
-        graph.dfs(startV);
 
-        int[] visitOrderArr = graph.getVisitOrderArr();
+        for (int i = 0; i < nE; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            dfsGraph[x].add(y);
+            dfsGraph[y].add(x);
+        }
+
+        for (int i = 0; i <= nV; i++) {
+            Collections.sort(dfsGraph[i]);
+        }
+
+        dfs(startV);
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= nV; i++) {
+        for (int i = 1; i <= nV ; i++) {
             sb.append(visitOrderArr[i]).append("\n");
         }
 
         System.out.println(sb);
+
+
     }
 
-}
-
-class DfsGraph {
-
-    private int nV;
-    private int visitOrder;
-    private boolean[][] dfsGraph; // 모든 간선의 cost = 1이므로.
-    private boolean[] visitedArr;
-    private int[] visitOrderArr;
-
-    public DfsGraph(int nV) {
-        this.nV = nV;
-        visitOrder = 1;
-        dfsGraph = new boolean[nV+1][nV+1];
-        visitedArr = new boolean[nV+1];
-        visitOrderArr = new int[nV+1];
-    }
-
-    public void put(int x, int y) { dfsGraph[x][y] = dfsGraph[y][x] = true; }
-
-    public int[] getVisitOrderArr() { return visitOrderArr; }
-
-    public void dfs(int currentVector) {
-        visitedArr[currentVector] = true;
+    static void dfs(int currentVector) {
         visitOrderArr[currentVector] = visitOrder++;
+        visitedArr[currentVector] = true;
 
-        for (int i = 1; i <= nV; i++) {
-            if (dfsGraph[currentVector][i] == true && visitedArr[i] == false) dfs(i);
+        for(int nextVector : dfsGraph[currentVector]) { // 모든 노드가 아니라, 간선정보 입력으로 추가된 것들만 탐색하면 됨.
+            if (visitedArr[nextVector] == false) dfs(nextVector);
         }
     }
 }
